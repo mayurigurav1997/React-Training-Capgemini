1902b89a9d52db86c21927b02d071bff51756cb3
Mayuri <mayurigurav1997@gmail.com>
2024-02-20 14:39:55 +0530

    assignment 4 completed

---
+++ Day-1/Assignment 4/index.js
@@ -0,0 +1,13 @@
+function showName() {
+    // console.log(id)
+    // var buttonName =  document.getElementById(id).innerHTML;
+    // document.getElementById("output").innerHTML = buttonName;
+    const buttons = document.querySelectorAll("button");
+    console.log(buttons)
+    buttons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            console.log(event.target.textContent)
+            output.textContent =  event.target.textContent;
+        });
+    });
+}
