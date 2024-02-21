ac3cbed7e22f054cc786651df5f537788ebb949d
Mayuri <mayurigurav1997@gmail.com>
2024-02-21 10:13:23 +0530

    social media links added

---
+++ Day-2/CaseStudy/index.js
@@ -0,0 +1,10 @@
+// document.getElementById("signin").addEventListener("click", function() {
+//     window.location.href = "http://127.0.0.1:5500/Day-2/CaseStudy/signin.html"; 
+// });
+document.getElementById("login").addEventListener("click", function() {
+    window.location.href = "./login.html"; 
+});
+document.getElementById("signin").addEventListener("click", function() {
+    window.location.href = "./signin.html"; 
+});
+
