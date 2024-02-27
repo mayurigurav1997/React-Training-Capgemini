c601342c2d97f382220ac21d3a07f2a39aea6fac
Mayuri <mayurigurav1997@gmail.com>
2024-02-27 16:03:58 +0530

    form in react

--- Day-7/demo/src/component/WishMe.jsx
+++ Day-7/demo/src/component/WishMe.jsx
@@ -1,25 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const WishMe = () => {
-    let [counter, setCounter] = useState(100)
-    let[msg, setMsg]= useState("Happy Holi!")
-    // console.log("Inside the wish me")
-    let incCount = ()=>{
-        setCounter(counter + 1)
-        // console.log("Inside the counter")
+  let [counter, setCounter] = useState(100);
+  let [msg, setMsg] = useState("Happy Holi!");
+  let [fullName, setFullName] = useState({ fn: "", ln: "" });
+  // console.log("Inside the wish me")
+  let incCount = () => {
+    setCounter(counter + 1);
+    // console.log("Inside the counter")
+  };
+  let desCount = () => {
+    setCounter(counter - 1);
+  };
+  let handleChange = (event) => {
+    let { name, value } = event.target;
+    if (name === "fn") {
+      setFullName({ ...fullName, fn: value });
+    } else {
+      setFullName({ ...fullName, ln: value });
     }
-    let desCount = ()=>{
-        setCounter(counter - 1)
-    }
+  };
   return (
     <div>
-        <div>----- UI render by functional component ----</div>
-            <h3>{msg}</h3>
-          <button onClick={incCount}>Increment</button>
-          Counter:{counter}
-          <button onClick={desCount}>Decrement</button>
-        </div>
-  )
-}
+      <div>----- UI render by functional component ----</div>
+      <h3>{msg}</h3>
+      <button onClick={incCount}>Increment</button>
+      Counter:{counter}
+      <button onClick={desCount}>Decrement</button> <br />
+      FirstName:
+      <input type="text" name="fn" onChange={handleChange} /> <br />
+      LastName:
+      <input type="text" name="ln" onChange={handleChange} />
+      <h3>
+        FirstName:{fullName.fn} - LastName:{fullName.ln}
+      </h3>
+    </div>
+  );
+};
 
-export default WishMe
+export default WishMe;
