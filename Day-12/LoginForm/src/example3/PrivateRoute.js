9773930004ca959bbcff8b59dda920608499d4d9
Mayuri <mayurigurav1997@gmail.com>
2024-03-05 18:03:53 +0530

    code cleanup

--- Day-12/LoginForm/src/example3/PrivateRoute.js
+++
@@ -1,7 +0,0 @@
-
-
-let PrivateRoute = ({children}) => {
-    return sessionStorage.getItem("user")==="admin"?children:<h3 className="text-danger">Restricted page.</h3>
-}
-
-export default PrivateRoute;
